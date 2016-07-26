0a119d44df35ad99e5a64624d364ec124704a16b
phanduong2211 <phanduong2211@gmail.com>
2016-07-26 21:12:45 +0700

    detail app pages
    detail app pages

--- resources/views/fontend/app/detailapp.blade.php
+++ resources/views/fontend/app/detailapp.blade.php
@@ -1,52 +1,60 @@
 @extends('fontend.layout_qc')
 @section('box_center')
 <div class="box_sales">
+@if(count($detailapp)>0)
       <div class="box_title">
         <aside>
-          <label>Kho ứng dụng</label>
+          <label>{{$detailapp[0]->name}}</label>
           <span></span> </aside>
       </div>
       <div class="ungdungdetails fl_top30">
       	<div class="ungdung_img">
-        	<figure><img src="http://dangcapdigital.com/images/apps/51-kodi-161-jarvisexe.jpg" alt="kodi 16.1-Jarvis.exe"></figure>
+        	<figure><img src="{{Asset('')}}public/kingtech/images/apps/{{$detailapp[0]->image}}" alt="{{$detailapp[0]->name}}"></figure>
             <aside>
-            	<h2>kodi 16.1-Jarvis.exe</h2>
+            	<h2>{{$detailapp[0]->name}}</h2>
                 <table width="100%" border="1">
                   <tbody><tr>
                     <td>Tình trạng</td>
-                    <td>Miễn Phí</td>
+                    <td>{{$detailapp[0]->status}}</td>
                   </tr>
                   <tr>
                     <td>Thể loại</td>
-                    <td><a href="http://dangcapdigital.com/kho-ung-dung/25-kodi-dangcapdigitalexe" title="kodi Dangcapdigital.EXE">kodi Dangcapdigital.EXE</a></td>
+                    @foreach($cateApps as $cateapp)
+                    @if($detailapp[0]->cate_id==$cateapp->id)
+                    <td><a href="{{Asset('')}}app/{{$cateapp->id.'-'.$cateapp->url}}" title="{{$cateapp->name}}">{{$cateapp->name}}</a></td>
+                    @endif
+                    @endforeach
                   </tr>
                   <tr>
                     <td>Dung lượng</td>
-                    <td>83 Mb</td>
+                    <td>{{$detailapp[0]->capacity}}</td>
                   </tr>
                   <tr>
                     <td>Yêu cầu</td>
-                    <td>Android 4.0</td>
+                    <td>{{$detailapp[0]->require}}</td>
                   </tr>
                   <tr>
                     <td>Phiên bản hiện tại</td>
-                    <td>1.0</td>
+                    <td>{{$detailapp[0]->version}}</td>
                   </tr>
                   <tr>
                     <td>Nhóm phát triển</td>
-                    <td>Dangcapdigital.com</td>
+                    <td>{{$detailapp[0]->developers}}</td>
                   </tr>
                   <tr>
-                    <td colspan="2"><a href="http://kodi.tivibox.vn/APP/kodi-16.1-Jarvis.exe" target="_parent" class="label-info">Tải về</a></td>
+                    <td colspan="2"><a href="{{$detailapp[0]->app_url}}" target="_parent" class="label-info">Tải về</a></td>
                   </tr>
                 </tbody></table>
 
             </aside>
             <big>
-            	kodi Dangcapdigital 16.1-Jarvis.exe dành cài đặt cho máy tính hệ điều hành windows            </big>
+            	{{$detailapp[0]->content}}            </big>
         </div>
       	<div class="ungdung_text">
                     </div>    
       </div>
+      @elseif(count($detailapp)>0)
+        không tìm thấy ứng dụng yêu cầu
+@endif
     </div>
 @stop
